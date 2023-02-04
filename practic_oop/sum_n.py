eb453c15977c86c8b48800e165d6245b031dd782
Amadu <amaduhamzajaarah300@gmail.com>
2023-02-04 10:30:33 +0000

    return a summation of numner

---
+++ practic_oop/sum_n.py
@@ -0,0 +1,11 @@
+'''
+This program takes an input num from the user and passes it to the function sum_to_n, which returns the sum of the range of numbers from 1 to num using the built-in sum function. Finally, it prints the result.
+This program takes an input num from the user and passes it to the function sum_to_n, which returns the sum of the range of numbers from 1 to num using the built-in sum function. Finally, it prints the result. '''
+
+def sum_n(num):
+
+	return sum(range(1, num +1)
+
+num=int(input("Enter a number greater than zero :"))
+
+print("The summation of every number from 1 to", num, "is", sum_to_n(num))
